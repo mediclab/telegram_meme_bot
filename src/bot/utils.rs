0cccb082e04fda71bb38a9b5b32639a6970b9770
Dmitriy Kozhanov <medic8463@gmail.com>
2023-01-16 17:50:27 +0300

    Fix meme top query

--- src/bot/utils.rs
+++ src/bot/utils.rs
@@ -1,4 +1,4 @@
-use chrono::{DateTime, Datelike, Days, Timelike, Utc};
+use chrono::{DateTime, Datelike, Days, TimeZone, Utc};
 use now::DateTimeNow;
 use teloxide::types::User;
 
@@ -9,98 +9,131 @@ pub fn get_user_text(user: &User) -> String {
     }
 }
 
-pub fn last_workday_of_month() -> DateTime<Utc> {
-    let mut date = Utc::now().end_of_month();
-
-    date = match date.weekday().number_from_monday() {
-        6 => date.checked_sub_days(Days::new(1)).unwrap(),
-        7 => date.checked_sub_days(Days::new(2)).unwrap(),
-        _ => date,
-    };
-
-    date.with_hour(18)
-        .unwrap()
-        .with_minute(59)
-        .unwrap()
-        .with_second(59)
-        .unwrap()
+pub enum Period {
+    Week,
+    Month,
+    Year,
 }
 
-pub fn first_workday_of_month() -> DateTime<Utc> {
-    let mut date = Utc::now().beginning_of_month();
+impl Period {
+    pub fn dates(&self) -> (DateTime<Utc>, DateTime<Utc>) {
+        match *self {
+            Period::Week => Period::week_dates(),
+            Period::Month => Period::month_dates(),
+            Period::Year => Period::year_dates(),
+        }
+    }
 
-    date = match date.weekday().number_from_monday() {
-        6 => date.checked_sub_days(Days::new(1)).unwrap(),
-        7 => date.checked_sub_days(Days::new(2)).unwrap(),
-        1 => date.checked_sub_days(Days::new(3)).unwrap(),
-        _ => date,
-    };
+    fn week_dates() -> (DateTime<Utc>, DateTime<Utc>) {
+        let start_week = Utc::now()
+            .beginning_of_week()
+            .checked_sub_days(Days::new(3))
+            .unwrap();
 
-    date.with_hour(19)
-        .unwrap()
-        .with_minute(00)
-        .unwrap()
-        .with_second(00)
-        .unwrap()
-}
+        let start = Utc
+            .with_ymd_and_hms(
+                start_week.year(),
+                start_week.month(),
+                start_week.day(),
+                16,
+                0,
+                0,
+            )
+            .unwrap();
 
-pub fn first_workday_of_week() -> DateTime<Utc> {
-    Utc::now()
-        .beginning_of_week()
-        .checked_sub_days(Days::new(3))
-        .unwrap()
-        .with_hour(19)
-        .unwrap()
-        .with_minute(00)
-        .unwrap()
-        .with_second(00)
-        .unwrap()
-}
+        let end_week = Utc::now()
+            .beginning_of_week()
+            .checked_sub_days(Days::new(2))
+            .unwrap();
 
-pub fn last_workday_of_week() -> DateTime<Utc> {
-    Utc::now()
-        .end_of_week()
-        .checked_sub_days(Days::new(2))
-        .unwrap()
-        .with_hour(18)
-        .unwrap()
-        .with_minute(59)
-        .unwrap()
-        .with_second(59)
-        .unwrap()
-}
+        let end = Utc
+            .with_ymd_and_hms(
+                end_week.year(),
+                end_week.month(),
+                end_week.day(),
+                15,
+                59,
+                59,
+            )
+            .unwrap();
 
-pub fn last_workday_of_year() -> DateTime<Utc> {
-    let mut date = Utc::now().end_of_year();
+        (start, end)
+    }
 
-    date = match date.weekday().number_from_monday() {
-        6 => date.checked_sub_days(Days::new(1)).unwrap(),
-        7 => date.checked_sub_days(Days::new(2)).unwrap(),
-        _ => date,
-    };
+    fn month_dates() -> (DateTime<Utc>, DateTime<Utc>) {
+        let start_month = Period::get_first_work_day(&Utc::now().beginning_of_month());
 
-    date.with_hour(18)
-        .unwrap()
-        .with_minute(59)
-        .unwrap()
-        .with_second(59)
-        .unwrap()
-}
+        let start = Utc
+            .with_ymd_and_hms(
+                start_month.year(),
+                start_month.month(),
+                start_month.day(),
+                16,
+                0,
+                0,
+            )
+            .unwrap();
 
-pub fn first_workday_of_year() -> DateTime<Utc> {
-    let mut date = Utc::now().beginning_of_year();
+        let end_month = Period::get_last_work_day(&Utc::now().end_of_month());
 
-    date = match date.weekday().number_from_monday() {
-        6 => date.checked_sub_days(Days::new(1)).unwrap(),
-        7 => date.checked_sub_days(Days::new(2)).unwrap(),
-        1 => date.checked_sub_days(Days::new(3)).unwrap(),
-        _ => date,
-    };
+        let end = Utc
+            .with_ymd_and_hms(
+                end_month.year(),
+                end_month.month(),
+                end_month.day(),
+                15,
+                59,
+                59,
+            )
+            .unwrap();
 
-    date.with_hour(19)
-        .unwrap()
-        .with_minute(00)
-        .unwrap()
-        .with_second(00)
-        .unwrap()
+        (start, end)
+    }
+
+    fn year_dates() -> (DateTime<Utc>, DateTime<Utc>) {
+        let start_month = Period::get_first_work_day(&Utc::now().beginning_of_year());
+
+        let start = Utc
+            .with_ymd_and_hms(
+                start_month.year(),
+                start_month.month(),
+                start_month.day(),
+                16,
+                0,
+                0,
+            )
+            .unwrap();
+
+        let end_month = Period::get_last_work_day(&Utc::now().end_of_year());
+
+        let end = Utc
+            .with_ymd_and_hms(
+                end_month.year(),
+                end_month.month(),
+                end_month.day(),
+                15,
+                59,
+                59,
+            )
+            .unwrap();
+
+        (start, end)
+    }
+
+    fn get_last_work_day(date: &DateTime<Utc>) -> DateTime<Utc> {
+        match date.weekday().number_from_monday() {
+            6 => date.checked_sub_days(Days::new(1)).unwrap(),
+            7 => date.checked_sub_days(Days::new(2)).unwrap(),
+            _ => *date,
+        }
+    }
+
+    fn get_first_work_day(date: &DateTime<Utc>) -> DateTime<Utc> {
+        match date.weekday().number_from_monday() {
+            6 => date.checked_sub_days(Days::new(1)).unwrap(),
+            7 => date.checked_sub_days(Days::new(2)).unwrap(),
+            1 => date.checked_sub_days(Days::new(3)).unwrap(),
+            _ => *date,
+        }
+    }
 }
