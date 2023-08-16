eac16891427cffdd1496d5cfb4ee67906b3edc2f
Dmitry Kozhanov <Medic8463@gmail.com>
2023-08-17 01:50:05 +0300

    Build update (#33)

--- src/bot/commands.rs
+++ src/bot/commands.rs
@@ -1,7 +1,6 @@
 use std::sync::Arc;
 
 use anyhow::Result;
-use teloxide::types::{InlineKeyboardButton, InlineKeyboardMarkup};
 use teloxide::{prelude::*, utils::command::BotCommands};
 
 use crate::app::Application;
